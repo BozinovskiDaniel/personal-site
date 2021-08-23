ce416a06cc30b57df530ca5a97213e622bdda1d9
BozinovskiDaniel <BozinovskiDaniel@hotmail.com>
2021-08-24 07:58:31 +1000

    latestg

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,10 +3,10 @@ module.exports = {
     {
       resolve: "gatsby-theme-portfolio-minimal",
       options: {
-        siteUrl: "https://gatsby-starter-portfolio-minimal-theme.netlify.app/", // Used for sitemap generation
+        siteUrl: "https://bozinovskidaniel.com/", // Used for sitemap generation
         manifestSettings: {
-          favicon: "./content/images/favicon.png", // Path is relative to the root
-          siteName: "My Minimal Portfolio", // Used in manifest.json
+          favicon: "./content/images/mountain.png", // Path is relative to the root
+          siteName: "Daniel's Portfolio", // Used in manifest.json
           shortName: "Portfolio", // Used in manifest.json
           startUrl: "/", // Used in manifest.json
           backgroundColor: "#FFFFFF", // Used in manifest.json

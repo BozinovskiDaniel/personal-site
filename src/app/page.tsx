1e2f68e586d0088d757437f67f63d8697b9e3822
BozinovskiDaniel <BozinovskiDaniel@hotmail.com>
2024-05-01 13:53:19 +1000

    Try add favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,14 @@ import { FunctionComponent, memo } from 'react'
 
 import { Hero } from '@/components/hero'
 import { Particles } from '@/components/particles'
+import Head from 'next/head';
 
 const Page: FunctionComponent = memo(() => (
   <div className="h-full bg-black">
+    <Head>
+                <link rel="icon" href="/mountain.png" type="image/x-icon" />
+                {/* Other head elements */}
+            </Head>
     <Hero />
     <Particles />
   </div>
